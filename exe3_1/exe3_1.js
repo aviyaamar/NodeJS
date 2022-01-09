1a680f6feb624e4adddbd7b713cc48be2beb52e9
aviyaamar <aviyaamar1@gmail.com>
2022-01-09 22:02:31 +0200

    add exe3_1

---
+++ exe3_1/exe3_1.js
@@ -0,0 +1,16 @@
+//import bring one variable , what do you want bring. and require bring all the module 
+// Import is the new way in ES6 to get an external module in your code
+// Require is the way to do it in NodeJS
+
+// Importing babel to your js module file lets you import it using the import keyword
+ export const var1 = ()=>{
+     console.log('hi');
+ }
+
+ export const var2 = ()=>{
+    console.log('hi');
+}
+
+export const var3 = ()=>{
+    console.log('hi');
+}
---
+++ exe3_1/test.js
@@ -0,0 +1,5 @@
+import { var1, var2, var3 } from './exe3_1'
+
+var1();
+var2();
+var3();
